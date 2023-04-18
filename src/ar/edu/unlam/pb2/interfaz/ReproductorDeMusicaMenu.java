a0043585b9929b5c5ede93cfd55ec9d4cb3642ba
Celena Moscovich <celena.ark@gmail.com>
2023-04-18 18:03:55 -0300

    mostrar mensaje

--- src/ar/edu/unlam/pb2/interfaz/ReproductorDeMusicaMenu.java
+++ src/ar/edu/unlam/pb2/interfaz/ReproductorDeMusicaMenu.java
@@ -3,8 +3,18 @@ package ar.edu.unlam.pb2.interfaz;
 public class ReproductorDeMusicaMenu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//MENU
+		
+		mostrarMensaje("Welcome");
+		
+		
 
+	}
+
+	private static void mostrarMensaje(String mensaje) {
+		System.out.println(mensaje);
+		
 	}
 
 }
