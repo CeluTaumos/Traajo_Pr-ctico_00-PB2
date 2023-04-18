06835e04b267f91ff9943bf4db64faec6a0f8e80
Micaela Zara <zara_micaela@outlook.com>
2023-04-18 20:29:54 -0300

    test

--- Test/ar/edu/unlam/pb2/interfazSpotify/ReproductorDeMusicaMenuTest.java
+++ Test/ar/edu/unlam/pb2/interfazSpotify/ReproductorDeMusicaMenuTest.java
@@ -5,38 +5,76 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ar.edu.unlam.pb2.dominioSpotify.Sistema;
+import ar.edu.unlam.pb2.dominioSpotify.Usuarios;
 
 public class ReproductorDeMusicaMenuTest {
 
 	@Test
 	public void queSeHayaCreadoElObjetoTipoSistema() {
 //		fail("Not yet implemented");
-		
-		//datos de entrada
+
+		// datos de entrada
 		Sistema spotyPirata;
-		
-		//ejecucion
-		spotyPirata = new Sistema("spotyPirata",5,10);
-		
-		//verificacion
-		
+
+		// ejecucion
+		spotyPirata = new Sistema("spotyPirata", 5, 10);
+
+		// verificacion
+
 		assertNotNull(spotyPirata);
-		
+
 	}
+
+	@Test
 	public void queSeHayanGuardadoLosDatosCorrectamente() {
-				//datos de entrada
-				String nombre = "pepito";
-				String contra = "contrasenia";
-				String correo = "hola@estoesunaprueba.com";
-				Sistema spotyPirata;
-				
-				//ejecucion
-				spotyPirata = new Sistema("spotyPirata",5,10);
-				assertTrue(spotyPirata.guardarDatos(nombre, correo, contra));
-				
-				//verificacion
-				
-				assertNotNull(spotyPirata);
-		
+		// datos de entrada
+		String nombre = "pepito";
+		String contra = "contrasenia";
+		String correo = "hola@estoesunaprueba.com";
+		Sistema spotyPirata;
+
+		// ejecucion
+		spotyPirata = new Sistema("spotyPirata", 5, 10);
+		assertTrue(spotyPirata.guardarDatos(nombre, correo, contra));
+
+		// verificacion
+
+		assertNotNull(spotyPirata);
+
+	}
+
+	@Test
+	public void queElNombreDelUsuarioSeHayaGuardadoCorrectamente() {
+		// datos de entrada
+		Sistema spotyPirata;
+		Usuarios user;
+		String nombre = "pepito";
+		String contra = "contrasenia";
+		String correo = "hola@estoesunaprueba.com";
+
+		// ejecucion
+		spotyPirata = new Sistema("spotyPirata", 5, 10);
+		user = new Usuarios(nombre, contra, correo);
+
+		assertEquals(user.getNombreDelUsuario(), nombre);
+
+	}
+
+	@Test
+	public void queElNombreDelUsuarioSeHayaGuardadoIncorrectamente() {
+		// datos de entrada
+		Sistema spotyPirata;
+		Usuarios user;
+		String nombre = "pepito";
+		String contra = "contrasenia";
+		String correo = "hola@estoesunaprueba.com";
+		String nombreMalo = "mica";
+
+		// ejecucion
+		spotyPirata = new Sistema("spotyPirata", 5, 10);
+		user = new Usuarios(nombre, contra, correo);
+
+		assertEquals(user.getNombreDelUsuario(), nombreMalo);
+
 	}
 }
