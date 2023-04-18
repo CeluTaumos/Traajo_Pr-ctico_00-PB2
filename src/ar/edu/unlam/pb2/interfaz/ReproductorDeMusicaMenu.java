cecb6d21ae37367354391ddc9d8f355f8b295cb5
Melina <violamelina.b@gmail.com>
2023-04-18 18:05:31 -0300

    Modificacion de prueba

--- src/ar/edu/unlam/pb2/interfaz/ReproductorDeMusicaMenu.java
+++ src/ar/edu/unlam/pb2/interfaz/ReproductorDeMusicaMenu.java
@@ -3,8 +3,7 @@ package ar.edu.unlam.pb2.interfaz;
 public class ReproductorDeMusicaMenu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola");
 	}
 
 }
